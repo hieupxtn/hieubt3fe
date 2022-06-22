9bcbda47c847e6af6d82ee7e43bccfb6826850a0
hieupxtn <83078842+hieupxtn@users.noreply.github.com>
2022-06-22 09:59:06 +0700

    new

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import React, {Fragment} from 'react';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 
 const App = () => (
-<Router>
-      <Fragment>
-          <Navbar />
-          <Landing />
-      </Fragment>
-      <h1>Hello</h1>
-</Router>
+<BrowserRouter>
+    <Fragment>
+      <Navbar />
+      <Route  path='/' element={<Landing />} />
+      <Routes>
+        <Route path='/login' element = {<Login />} />
+        <Route path='register' element = {<Register />} />
+      </Routes>
+    </Fragment>
+</BrowserRouter>
 );
 
 export default App;
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,25 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div>
-        <nav className='navbar bg-dark'>
-            <h1>
-                <a href='index.html'><i className='fas fa-code'></i> DevConnector</a>
-            </h1>
-            <ul>
-                <li>
-                    <a href='profiles.html'>Developers</a>
-                </li>
-                <li>
-                    <a href='register.html'>Register</a>
-                </li>
-                <li>
-                    <a href='login.html'>Login</a>
-                </li>
-            </ul>
-        </nav>
-    </div>
+    <nav classNameName="navbar bg-dark">
+      <h1>
+        <Link to="/"><i className="fas fa-code"></i> DevConnector</Link>
+      </h1>
+      <ul>
+        <li><Link to="profiles">Developers</Link></li>
+        <li><a href="register.html">Register</a></li>
+        <li><a href="login.html">Login</a></li>
+      </ul>
+    </nav>
   )
 }
 
